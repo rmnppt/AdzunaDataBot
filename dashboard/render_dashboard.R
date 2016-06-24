281253e7740f3381c329b108975990f61ba0cb8f
rmnppt <romanpopat@gmx.com>
2016-06-24 16:17:52 +0000

    added automated generation of html dashboard and syncing with s3 bucket

---
+++ dashboard/render_dashboard.R
@@ -0,0 +1,5 @@
+library(rmarkdown)
+rmarkdown::render(
+  input = "/home/rpopat/AdzunaDataBot/dashboard/AdzunaFlexDashboard.Rmd", 
+  output_file = "/home/rpopat/AdzunaDataBot/dashboard/AdzunaFlexDashboard.html",
+)
